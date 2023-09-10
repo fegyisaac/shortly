1e95c329e4e95663402f41bebc7a9e4a95a4f0f8
zicojr <fegyisaac160@gmail.com>
2023-09-10 21:32:02 +0100

    finish card setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,27 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    xs: "375px",
-    // => @media (min-width: 640px) { ... }
+    screens: {
 
-    sm: "480px",
-    // => @media (min-width: 640px) { ... }
-
-    md: "640px",
-    // => @media (min-width: 768px) { ... }
-
-    lg: "1024px",
-    // => @media (min-width: 1024px) { ... }
-
-    xl: "1280px",
-    // => @media (min-width: 1280px) { ... }
-
-    "2xl": "1536px",
-    // => @media (min-width: 1536px) { ... }
+      "xs": "375px",
+      "sm": "480px",
+      "md": "640px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
 
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
-
     extend: {
       colors: {
         Primary: "hsl(180, 66%, 49%)",
-        DViolet_900: "hsl(257, 27%, 26%)",
+        DViolet_600: "hsl(257, 27%, 26%)",
         Secondary: "hsl(0, 87%, 67%)",
-        Gray_10: "hsl(0, 0%, 75%)",
+        Gray_10: "hsl(0, 0%, 89%)",
+        Gray_40: "hsl(0, 0%, 70%)",
+        Gray_60: "hsl(0, 0%, 51%)",
         GViolet_50: "hsl(257, 7%, 63%)",
         DBlue_900: "hsl(255, 11%, 22%)",
         DViolet_900: "hsl(260, 8%, 14%)",
